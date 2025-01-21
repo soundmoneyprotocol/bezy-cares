a4c17733e3aa8ea5e52c86e76c35231624915634
Abel Osaretin <contact.abel321@gmail.com>
2025-01-21 20:28:17 +0100

    feat: Organized and formated test file

--- test/cares.ts
+++ test/cares.ts
@@ -1,126 +1,202 @@
 import {
     time,
-    loadFixture,
-  } from "@nomicfoundation/hardhat-toolbox/network-helpers";
-  import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
-  import { expect } from "chai";
-  import hre from "hardhat";
-  import { ethers } from "hardhat";
+    loadFixture
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import hre from "hardhat";
+import { ethers } from "hardhat";
 
-  describe("Cares.Sol", function () {
-
+describe("Cares.Sol", function () {
     async function deployCares() {
-    
         // Contracts are deployed using the first signer/account by default
         const [owner, otherAccount] = await hre.ethers.getSigners();
 
-        // Test Token Deployment 
-        const token = await hre.ethers.getContractFactory("TestToken")
+        // Test Token Deployment
+        const token = await hre.ethers.getContractFactory("TestToken");
         const tokenContract = await token.deploy();
 
         // Hope Contract Deployment
         const cares = await hre.ethers.getContractFactory("Hopes");
-        const caresContract = await cares.deploy(owner,tokenContract.getAddress());
-    
+        const caresContract = await cares.deploy(
+            owner,
+            tokenContract.getAddress()
+        );
+
         return { caresContract, tokenContract, owner, otherAccount };
-      }
+    }
 
-
-      describe("Deployment", function () {
+    describe("Deployment", function () {
         it("Should deploy correctly", async function () {
-            const { caresContract,owner, tokenContract } = await loadFixture(deployCares);
-        
+            const { caresContract, owner, tokenContract } = await loadFixture(
+                deployCares
+            );
+
             const totalSupply = await tokenContract.totalSupply();
             const tokenAddress = await tokenContract.getAddress();
-        
+
             expect(await tokenContract.balanceOf(owner)).to.equal(totalSupply);
             expect(await caresContract.OWNER()).to.equal(owner);
-            expect(await caresContract.BEZY_TOKEN_CONTRACT()).to.equal(tokenAddress);
-          });
-    
-        it("Should add new Campaign", async function () {
-          const { owner,caresContract, otherAccount } = await loadFixture(deployCares);
+            expect(await caresContract.BEZY_TOKEN_CONTRACT()).to.equal(
+                tokenAddress
+            );
+        });
+    });
 
-          const uri = ethers.encodeBytes32String("campaignURI");
-          const goal = ethers.parseEther("10");
+    describe("Adding Campaign", function () {
+        it("Should add new Campaign, Emit Event and Assert Campaign Information", async function () {
+            const { owner, caresContract, otherAccount } = await loadFixture(
+                deployCares
+            );
 
-          // Revert 
-          await expect(caresContract.addCampaign(uri,0,otherAccount)).to.be.revertedWithCustomError(caresContract,"Hopes__NoGoalSet")
+            const uri = ethers.encodeBytes32String("campaignURI");
+            const goal = ethers.parseEther("10");
+
+            // Revert
+            await expect(
+                caresContract.addCampaign(uri, 0, otherAccount)
+            ).to.be.revertedWithCustomError(caresContract, "Hopes__NoGoalSet");
 
             // Event Emission
-          await expect(caresContract.addCampaign(uri, goal, otherAccount))
-          .to.emit(caresContract, "CampaignCreated"); 
+            await expect(
+                caresContract.addCampaign(uri, goal, otherAccount)
+            ).to.emit(caresContract, "CampaignCreated");
 
-          // Assertions
+            // Assertions
 
-          const campaign = await caresContract.s_Campaigns(0);
+            const campaign = await caresContract.s_Campaigns(0);
 
-        expect(campaign.id).to.equal(0);
-      expect(campaign.metadata).to.equal(uri);
-      expect(campaign.creator).to.equal(owner);
-      expect(campaign.goal).to.equal(goal);
-      expect(campaign.beneficiary).to.equal(otherAccount);
-      expect(campaign.totalAccrued).to.equal(0);
-
-      
-
-
+            expect(campaign.id).to.equal(0);
+            expect(campaign.metadata).to.equal(uri);
+            expect(campaign.creator).to.equal(owner);
+            expect(campaign.goal).to.equal(goal);
+            expect(campaign.beneficiary).to.equal(otherAccount);
+            expect(campaign.totalAccrued).to.equal(0);
         });
-    
-        it("Should fund a Campaign", async function () {
-          const { caresContract, owner, tokenContract, otherAccount } = await loadFixture(
-            deployCares
-          );
-
-          const uri = ethers.encodeBytes32String("campaignURI");
-          const goal = ethers.parseEther("10");
-          const amount = ethers.parseEther("1"); 
-
-          await expect(caresContract.addCampaign(uri, goal, otherAccount))
-          .to.emit(caresContract, "CampaignCreated");
-
-           // Revert 
-           await expect(caresContract.fundCampaign(0,0)).to.be.revertedWithCustomError(caresContract,"Hopes__InsufficientFunds")
-
-           const caresAddress = await caresContract.getAddress();
-
-           await tokenContract.connect(owner).approve(caresAddress, amount);
-
-           // Event Emission
-           await expect(
-            caresContract.connect(owner).fundCampaign(0, amount)
-          ).to.emit(caresContract, "CampaignFunded");
-
-          // Assertation
-
-          const campaign = await caresContract.s_Campaigns(0);
-          expect(campaign.totalAccrued).to.equal(amount);  
-        
-        });
-    
-        it("Should test token address change", async function () {
-            const { caresContract, owner, tokenContract, otherAccount } = await loadFixture(
+        it("Should revert when adding zero goal amount to new Campaign", async function () {
+            const { owner, caresContract, otherAccount } = await loadFixture(
                 deployCares
-              );
+            );
 
-               // Deploying a new token contract to replace the Bezy
-      const NewToken = await ethers.getContractFactory("TestToken");
-      const newToken = await NewToken.deploy();
+            const uri = ethers.encodeBytes32String("campaignURI");
 
-      const newTokenAddress = await newToken.getAddress();
-      
-    // Revert when not owner
-    await expect (caresContract.connect(otherAccount).updateTokenContract(newTokenAddress)).to.be.revertedWithCustomError(caresContract, "Hopes__Unauthorized");
-
-      // Owner updates the token contract
-      await caresContract.connect(owner).updateTokenContract(newTokenAddress);
-
-      // Assertion
-      
-       expect( await caresContract.BEZY_TOKEN_CONTRACT()).to.equal(newTokenAddress);
-          
+            // Revert
+            await expect(
+                caresContract.addCampaign(uri, 0, otherAccount)
+            ).to.be.revertedWithCustomError(caresContract, "Hopes__NoGoalSet");
         });
-      });
-    
+    });
 
-  });
+    describe("Funding Campaign", function () {
+        it("Should fund Campaign, Emit Event, Assert funding amount", async function () {
+            const { caresContract, owner, tokenContract, otherAccount } =
+                await loadFixture(deployCares);
+
+            const uri = ethers.encodeBytes32String("campaignURI");
+            const goal = ethers.parseEther("10");
+            const amount = ethers.parseEther("1");
+
+            await expect(
+                caresContract.addCampaign(uri, goal, otherAccount)
+            ).to.emit(caresContract, "CampaignCreated");
+
+            // Revert
+            await expect(
+                caresContract.fundCampaign(0, 0)
+            ).to.be.revertedWithCustomError(
+                caresContract,
+                "Hopes__InsufficientFunds"
+            );
+
+            const caresAddress = await caresContract.getAddress();
+
+            await tokenContract.connect(owner).approve(caresAddress, amount);
+
+            // Event Emission
+            await expect(
+                caresContract.connect(owner).fundCampaign(0, amount)
+            ).to.emit(caresContract, "CampaignFunded");
+
+            // Assertation
+
+            const campaign = await caresContract.s_Campaigns(0);
+            expect(campaign.totalAccrued).to.equal(amount);
+        });
+
+        it("Should revert campaign funding with zero amount", async function () {
+            const { caresContract, owner, tokenContract, otherAccount } =
+                await loadFixture(deployCares);
+
+            const uri = ethers.encodeBytes32String("campaignURI");
+            const goal = ethers.parseEther("10");
+            const amount = ethers.parseEther("1");
+
+            await expect(
+                caresContract.addCampaign(uri, goal, otherAccount)
+            ).to.emit(caresContract, "CampaignCreated");
+
+            // Revert
+            await expect(
+                caresContract.fundCampaign(0, 0)
+            ).to.be.revertedWithCustomError(
+                caresContract,
+                "Hopes__InsufficientFunds"
+            );
+        });
+    });
+
+    describe("Changing Token", function () {
+        it("Should assert token address change", async function () {
+            const { caresContract, owner, tokenContract, otherAccount } =
+                await loadFixture(deployCares);
+
+            // Deploying a new token contract to replace the Bezy
+            const NewToken = await ethers.getContractFactory("TestToken");
+            const newToken = await NewToken.deploy();
+
+            const newTokenAddress = await newToken.getAddress();
+
+            // Revert when not owner
+            await expect(
+                caresContract
+                    .connect(otherAccount)
+                    .updateTokenContract(newTokenAddress)
+            ).to.be.revertedWithCustomError(
+                caresContract,
+                "Hopes__Unauthorized"
+            );
+
+            // Owner updates the token contract
+            await caresContract
+                .connect(owner)
+                .updateTokenContract(newTokenAddress);
+
+            // Assertion
+
+            expect(await caresContract.BEZY_TOKEN_CONTRACT()).to.equal(
+                newTokenAddress
+            );
+        });
+
+        it("Should revert if not owner changing token address", async function () {
+            const { caresContract, owner, tokenContract, otherAccount } =
+                await loadFixture(deployCares);
+
+            // Deploying a new token contract to replace the Bezy
+            const NewToken = await ethers.getContractFactory("TestToken");
+            const newToken = await NewToken.deploy();
+
+            const newTokenAddress = await newToken.getAddress();
+
+            // Revert when not owner
+            await expect(
+                caresContract
+                    .connect(otherAccount)
+                    .updateTokenContract(newTokenAddress)
+            ).to.be.revertedWithCustomError(
+                caresContract,
+                "Hopes__Unauthorized"
+            );
+        });
+    });
+});
